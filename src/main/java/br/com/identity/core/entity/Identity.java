add829c7baab5cc0c36c7cde707a6b5b5076581c
robsonbittencourt <robson.luizv@gmail.com>
2015-07-28 22:26:56 -0300

    Omitted table name in the annotation

--- src/main/java/br/com/identity/core/entity/Identity.java
+++ src/main/java/br/com/identity/core/entity/Identity.java
@@ -11,7 +11,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="identity")
+@Table
 public class Identity implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
